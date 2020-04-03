c044dfe087017d9c3240720e1df8791e97f092c8
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2020-04-03 14:48:03 +0200

    Implemented another button!

--- MJOLNIR-GUI.py
+++ MJOLNIR-GUI.py
@@ -48,6 +48,10 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         
         
+        
+        self.ui.DataSet_convertData_button.clicked.connect(self.DataSet_convertData_button_function)
+        
+        
         self.ui.pushButton_12.clicked.connect(self.testTitle)
         
         self.ui.View3D_plot_button.clicked.connect(self.View3D_plot_button_function)
@@ -56,15 +60,31 @@ class mywindow(QtWidgets.QMainWindow):
     def testTitle(self):
         
         print("It's working!")
-    def View3D_plot_button_function(self):
         
+        
+    def DataSet_convertData_button_function(self):
+        #The loading should be moved to a different button        
         fileList,_ = QtWidgets.QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","hdf Files (*.hdf);;All Files (*)")
-        
-        ds = GuiDataSet(fileList)
+        self.fileList=fileList
+
+        ds = GuiDataSet(self.fileList)
         ds.convertDataFile(binning=8,saveFile=False)
-        V = ds.View3D(0.02,0.02,0.1)
-        V.caxis = (0,1e-6)
+        self.ds=ds
         
+    def View3D_plot_button_function(self):
+
+        # Check if we already have data, otherwise convert current data.
+        if not hasattr(self, 'fileList'):
+            self.DataSet_convertData_button_function(self)
+        
+        V = self.ds.View3D(0.02,0.02,0.1)
+        
+        # This should be replaced with a call to the future set c axis function
+        V.caxis = (0,1e-6)
+        self.V=V
+        
+
+
 
 app = QtWidgets.QApplication([])
 
