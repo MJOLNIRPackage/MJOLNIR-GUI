76d590ced7df9c5508f649bd456586833b9fb9c1
Jakob Lass <lass.jakob@gmail.com>
2020-04-08 08:11:00 +0200

    Add tooltips, icons an other niceness

--- MJOLNIR-GUI.py
+++ MJOLNIR-GUI.py
@@ -18,7 +18,7 @@ from os import path
 plt.ion()
 
 
-from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5 import QtWidgets, QtCore, QtGui, Qt
 
 from MJOLNIR_GUI_ui import Ui_MainWindow  
 from MJOLNIR_Data import GuiDataFile,GuiDataSet
@@ -47,7 +47,6 @@ class mywindow(QtWidgets.QMainWindow):
     
         self.ui.setupUi(self)
         
-        self.currentDataSetIndex = 0
 
         self.dataSets = []
         
@@ -78,7 +77,8 @@ class mywindow(QtWidgets.QMainWindow):
         self.ui.QPlane_SetTitle_button.clicked.connect(self.QPlane_SetTitle_button_function)
         
         
-        
+        self.setupMenu()
+
  
         
     def DataSet_convertData_button_function(self):
@@ -86,8 +86,16 @@ class mywindow(QtWidgets.QMainWindow):
             
         binning=int(self.ui.DataSet_binning_comboBox.currentText())
         ds = self.DataSetModel.getCurrentDataSet()
-        ds.convertDataFile(binning=binning,saveFile=False)
-        
+        try:
+            ds.convertDataFile(binning=binning,saveFile=False)
+        except AttributeError as e:
+            msg = QtWidgets.QMessageBox(QtWidgets.QMessageBox.Critical,title='Error')
+            msg.setText("Error")
+            msg.setInformativeText(str(e))
+            msg.resize(300,200)
+            msg.exec_()
+
+        self.DataFileModel.layoutChanged.emit()
         
     def View3D_plot_button_function(self):
 
@@ -159,21 +167,37 @@ class mywindow(QtWidgets.QMainWindow):
 
     def setupDataSet(self): # Set up main features for Gui regarding the dataset widgets
         self.ui.DataSet_convertData_button.clicked.connect(self.DataSet_convertData_button_function)
+        self.ui.DataSet_convertData_button.setToolTip('Convert selected Dataset')
+        self.ui.DataSet_convertData_button.setStatusTip(self.ui.DataSet_convertData_button.toolTip())
+
         self.ui.DataSet_NewDataSet_button.clicked.connect(self.DataSet_NewDataSet_button_function)
+        self.ui.DataSet_NewDataSet_button.setToolTip('Add new Dataset')
+        self.ui.DataSet_NewDataSet_button.setStatusTip(self.ui.DataSet_NewDataSet_button.toolTip())
+
         self.ui.DataSet_DeleteDataSet_button.clicked.connect(self.DataSet_DeleteDataSet_button_function)
+        self.ui.DataSet_DeleteDataSet_button.setToolTip('Delete selected Dataset')
+        self.ui.DataSet_DeleteDataSet_button.setStatusTip(self.ui.DataSet_DeleteDataSet_button.toolTip())
+
         self.ui.DataSet_AddFiles_button.clicked.connect(self.DataSet_AddFiles_button_function)
+        self.ui.DataSet_AddFiles_button.setToolTip('Add new Datafiles')
+        self.ui.DataSet_AddFiles_button.setStatusTip(self.ui.DataSet_AddFiles_button.toolTip())
 
         self.DataSetModel = DataSetModel(dataSets=self.dataSets,DataSet_DataSets_listView=self.ui.DataSet_DataSets_listView)
         self.ui.DataSet_DataSets_listView.setModel(self.DataSetModel)
 
         self.ui.DataSet_DataSets_listView.clicked.connect(self.selectedDataSetChanged)
+
+        
         self.ui.DataSet_DataSets_listView.doubleClicked.connect(self.DataSet_DoubleClick_Selection_function)
 
     def setupDataFile(self): # Set up main features for Gui regarding the datafile widgets
         self.DataFileModel = DataFileModel(DataSet_filenames_listView=self.ui.DataSet_filenames_listView,dataSetModel=self.DataSetModel,DataSet_DataSets_listView=self.ui.DataSet_DataSets_listView)
         self.ui.DataSet_filenames_listView.setModel(self.DataFileModel)
         self.ui.DataSet_filenames_listView.clicked.connect(self.selectedDataFileChanged)
+
         self.ui.DataSet_DeleteFiles_button.clicked.connect(self.DataSet_DeleteFiles_button_function)
+        self.ui.DataSet_DeleteFiles_button.setToolTip('Delete selected Datafile')
+        self.ui.DataSet_DeleteFiles_button.setStatusTip(self.ui.DataSet_DeleteFiles_button.toolTip())
 
         self.ui.DataSet_DataSets_listView.doubleClicked.connect(self.DataFile_DoubleClick_Selection_function)
 
@@ -193,6 +217,7 @@ class mywindow(QtWidgets.QMainWindow):
 
     def DataSet_DeleteDataSet_button_function(self):
         self.DataSetModel.delete(self.ui.DataSet_DataSets_listView.selectedIndexes()[0])
+        self.DataFileModel.layoutChanged.emit()
         
     def DataSet_DeleteFiles_button_function(self):
         self.DataFileModel.delete()
@@ -209,11 +234,17 @@ class mywindow(QtWidgets.QMainWindow):
             folder=currentFolder
         else:
             folder = ""
-        files, _ = QtWidgets.QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", folder,"HDF (*.hdf);;All Files (*)")
+        files, _ = QtWidgets.QFileDialog.getOpenFileNames(self,"Open data Files", folder,"HDF (*.hdf);;NXS (*.nxs);;All Files (*)")
         if self.DataSetModel.getCurrentDatasetIndex() is None: # no dataset is currently selected
             self.DataSet_NewDataSet_button_function()
         self.DataFileModel.add(files)
-        
+
+    def setupMenu(self): # Set up all QActions and menus
+        self.ui.actionExit.setIcon(QtGui.QIcon('Icons/icons/cross-button.png'))
+        self.ui.actionExit.setToolTip('Exit the application') 
+        self.ui.actionExit.triggered.connect(self.close)
+
+
 
 # def run():
     
