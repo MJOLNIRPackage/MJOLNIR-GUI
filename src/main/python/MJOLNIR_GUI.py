92baf004894243f37f45fe5b3d8962464c8007f2
Jakob Lass <lass.jakob@gmail.com>
2020-04-15 16:27:10 +0200

    Update saving algorithm for settings

--- src/main/python/MJOLNIR_GUI.py
+++ src/main/python/MJOLNIR_GUI.py
@@ -646,7 +646,7 @@ class mywindow(QtWidgets.QMainWindow):
 
             localstring = [df.fileLocation if df.type != 'nxs' else df.original_file.fileLocation for df in ds]
             localstring.insert(0,ds.name)
-            saveString.append(' '.join(localstring))
+            saveString.append(';'.join(localstring))
             self.setProgressBarValue((i+1))
 
         dataSetString = ':'.join(saveString)
@@ -685,14 +685,14 @@ class mywindow(QtWidgets.QMainWindow):
         
         
         lines = dataSetString.split(':')
-        totalFiles = len(dataSetString.split(' ')) # Get estimate of total number of data files
+        totalFiles = len(dataSetString.split(';')) # Get estimate of total number of data files
         self.setProgressBarMaximum(totalFiles)
         counter = 0
 
         self.setProgressBarLabelText('Loading Data Sets and Files')
         for line in lines:
             
-            DSName,*files = line.split(' ')
+            DSName,*files = line.split(';')
             dfs = None
             if len(files)!=0: # If files in dataset, continue
                 dfs = []
