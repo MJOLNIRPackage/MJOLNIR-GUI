c801df300277ca25d044e49999d9b84ae13040ff
Jakob Lass <lass.jakob@gmail.com>
2020-04-17 16:23:07 +0200

    Addition of the Cut1D function
    Features not included: rlu, constantBins

--- src/main/python/MJOLNIR_GUI.py
+++ src/main/python/MJOLNIR_GUI.py
@@ -138,9 +138,11 @@ class mywindow(QtWidgets.QMainWindow):
 
 
         ##############################################################################
-        # Raw1Dcut
+        # 1Dcut
         ##############################################################################
         self.ui.Raw1D_plot_button.clicked.connect(self.Raw1D_plot_button_function)
+
+        self.ui.Cut1D_plot_button.clicked.connect(self.Cut1D_plot_button_funcion)
 
         self.setupMenu()
         self.setupStateMachine()
@@ -784,9 +786,38 @@ class mywindow(QtWidgets.QMainWindow):
 
         
         ax = ds.plot1D()
-        self.windows.append(ax)
+        self.windows.append(ax.get_figure())
         return True
         
+    @ProgressBarDecoratorArguments(runningText='Plotting Cut1D',completedText='Plotting Done')
+    def Cut1D_plot_button_funcion(self):
+        if not self.stateMachine.requireStateByName('Converted'):
+            return False
+
+        HStart = self.ui.Cut1D_HStart_lineEdit.text()
+        HEnd = self.ui.Cut1D_HEnd_lineEdit.text()
+        KStart = self.ui.Cut1D_KStart_lineEdit.text()
+        KEnd = self.ui.Cut1D_KEnd_lineEdit.text()
+        LStart = self.ui.Cut1D_LStart_lineEdit.text()
+        LEnd = self.ui.Cut1D_LEnd_lineEdit.text()
+
+        EMax = float(self.ui.Cut1D_EMax_lineEdit.text())
+        EMin = float(self.ui.Cut1D_EMin_lineEdit.text())
+
+        width = float(self.ui.Cut1D_Width_lineEdit.text())
+        minPixel = float(self.ui.Cut1D_MinPixel_lineEdit.text())
+
+        ds = self.DataSetModel.getCurrentDataSet()
+
+        q1 = np.array([HStart,KStart,LStart],dtype=float)
+        q2 = np.array([HEnd,KEnd,LEnd],dtype=float)
+
+        
+        ax,*_ = ds.plotCut1D(q1=q1,q2=q2,width=width,minPixel=minPixel,Emin=EMin,Emax=EMax,rlu=True,constantBins=False)
+        self.windows.append(ax.get_figure())
+        return True
+
+
     
     def setProgressBarValue(self,value):
         self.ui.progressBar.setValue(value)
