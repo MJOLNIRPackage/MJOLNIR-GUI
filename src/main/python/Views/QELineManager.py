b0ae6d1bad9439cb451f56fe787edc03e13dec03
Jakob Lass <lass.jakob@gmail.com>
2020-06-30 16:21:56 +0200

    Update text when units are changed

--- src/main/python/Views/QELineManager.py
+++ src/main/python/Views/QELineManager.py
@@ -31,8 +31,15 @@ def QELine_plot_button_function(self):
     KEnd=float(self.ui.QELine_KEnd_lineEdit.text())
     LEnd=float(self.ui.QELine_LEnd_lineEdit.text())
             
-    Q1 = np.array([HStart,KStart,LStart])
-    Q2 = np.array([HEnd,KEnd,LEnd])
+    if self.ui.QELine_SelectUnits_RLU_radioButton.isChecked():
+        rlu=True
+        Q1 = np.array([HStart,KStart,LStart])
+        Q2 = np.array([HEnd,KEnd,LEnd])
+    else:
+        rlu=False   
+        Q1 = np.array([HStart,KStart])
+        Q2 = np.array([HEnd,KEnd])
+   
     # Collect them into one array
     QPoints = np.array([Q1,Q2])
     
@@ -47,10 +54,6 @@ def QELine_plot_button_function(self):
     EnergyBins=np.linspace(EMin,EMax,NPoints)
 
     # Check various plot settings
-    if self.ui.QELine_SelectUnits_RLU_radioButton.isChecked():
-        rlu=True
-    else:
-        rlu=False        
 
     if self.ui.QELine_LogScale_checkBox.isChecked():
         log=True
@@ -90,7 +93,20 @@ def QELine_plot_button_function(self):
         _GUItools.dialog(text='QELine cut could not be made. Check the limits for the cut and try again!')
         return False
 
-
+def QELine_toggle_units_function(self):
+    if self.ui.QELine_SelectUnits_RLU_radioButton.isChecked(): # changed to RLU
+        # Change titles
+        self.ui.QELine_HLabel.setText('H')
+        self.ui.QELine_KLabel.setText('K')
+        self.ui.QELine_LLabel.setText('L')
+        self.ui.QELine_LStart_lineEdit.setEnabled(True)
+        self.ui.QELine_LEnd_lineEdit.setEnabled(True)
+    else: # Changing to AA
+        self.ui.QELine_HLabel.setText('Qx')
+        self.ui.QELine_KLabel.setText('Qy')
+        self.ui.QELine_LLabel.setText('N/A')
+        self.ui.QELine_LStart_lineEdit.setEnabled(False)
+        self.ui.QELine_LEnd_lineEdit.setEnabled(False)
 
 def QELine_setCAxis_button_function(self):       
     CAxisMin=float(self.ui.QELine_CAxisMin_lineEdit.text())
@@ -122,6 +138,7 @@ class QELineManager(QELineManagerBase, QELineManagerForm):
         self.guiWindow.QELine_plot_button_function = lambda: QELine_plot_button_function(self.guiWindow)
         self.guiWindow.QELine_setCAxis_button_function = lambda: QELine_setCAxis_button_function(self.guiWindow)
         self.guiWindow.QELine_SetTitle_button_function = lambda: QELine_SetTitle_button_function(self.guiWindow)
+        self.guiWindow.QELine_toggle_units_function = lambda: QELine_toggle_units_function(self.guiWindow)
 
         for key,value in self.__dict__.items():
             if 'QELine' in key:
@@ -132,3 +149,4 @@ class QELineManager(QELineManagerBase, QELineManagerForm):
         self.guiWindow.ui.QELine_plot_button.clicked.connect(self.guiWindow.QELine_plot_button_function)
         self.guiWindow.ui.QELine_setCAxis_button.clicked.connect(self.guiWindow.QELine_setCAxis_button_function)
         self.guiWindow.ui.QELine_SetTitle_button.clicked.connect(self.guiWindow.QELine_SetTitle_button_function)
+        self.guiWindow.ui.QELine_SelectUnits_RLU_radioButton.toggled.connect(self.guiWindow.QELine_toggle_units_function)
