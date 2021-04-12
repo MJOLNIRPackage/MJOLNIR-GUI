1d2bfaa9207850fa80d5897390579faf44e07437
Jakob Lass <lass.jakob@gmail.com>
2021-04-12 15:59:16 +0200

    Add build_settings method

--- src/main/python/AppContextEmulator.py
+++ src/main/python/AppContextEmulator.py
@@ -1,5 +1,4 @@
-import sys
-import os
+import sys,os,json
 class AppContextEmulator(object):
     def __init__(self,projectDirectory):
         self.projectDirectory = projectDirectory
@@ -10,12 +9,15 @@ class AppContextEmulator(object):
         elif local_os.lower() == 'win32' or local_os.lower() == 'cygwin':
             local_os = 'windows'
         elif local_os.lower() == 'darwin':
-            local_os = 'max'
+            local_os = 'mac'
         else:
             local_os = 'linux'
 
         self.os = local_os
         self.resourses = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','resources'))
+
+        self.build_settings = {}
+        self.get_build_settings()
 
 
     def get_resource(self,path):
@@ -27,3 +29,22 @@ class AppContextEmulator(object):
         else:
             return tryPath
             
+    def get_build_settings(self):
+        self.settingsFolder = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','..','build','settings'))
+
+        if not hasattr(self,'build_settings'):
+            self.build_settings = {}
+
+        baseFile = os.path.join(self.settingsFolder,'base.json')
+
+        with open(baseFile) as f:
+            newSettings = json.load(f)
+        self.build_settings = {**self.build_settings,**newSettings}
+
+        
+        specificFile = os.path.join(self.settingsFolder,self.os+'.json')
+        if os.path.exists(specificFile):
+            with open(specificFile) as f:
+                newSettings = json.load(f)
+                self.build_settings = {**self.build_settings,**newSettings}
+
