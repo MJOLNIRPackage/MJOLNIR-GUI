627d1b7f403c2f702de2830bb898e5ccbc725240
Jakob Lass <lass.jakob@gmail.com>
2021-06-11 14:19:28 +0200

    Add bibtex entry for citations Make hyperlink for About and Help

--- src/main/python/AboutDialog.py
+++ src/main/python/AboutDialog.py
@@ -11,18 +11,24 @@ class AboutDialog(QtWidgets.QDialog):
         self.setWindowTitle("About")
 
         with open(aboutFile) as f:
-            text = '\n'.join([line.replace('\n','') for line in f.readlines()])
+            text = "<br>".join([line.replace('\n','') for line in f.readlines()])
+
+        
         text = text.replace('{MJOLNIRGui}',version)
         text = text.replace('{MJOLNIR}',MJOLNIR.__version__)
+
 
         pythonVersion = '.'.join([str(x) for x in sys.version_info[:3]])
         text = text.replace('{PythonVersion}',pythonVersion)
         
-        self.about_label = QtWidgets.QLabel(text=text)
+        self.about_label = QtWidgets.QLabel(self)
+        self.about_label.setText(text)
         self.about_label.setWordWrap(True)
-        self.about_label.setTextInteractionFlags(QtCore.Qt.TextSelectableByMouse|QtCore.Qt.TextSelectableByKeyboard)
+        self.about_label.setTextInteractionFlags(QtCore.Qt.TextSelectableByMouse|QtCore.Qt.TextSelectableByKeyboard|
+                                                 QtCore.Qt.LinksAccessibleByKeyboard|QtCore.Qt.LinksAccessibleByMouse)
         
         self.about_label.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.about_label.setOpenExternalLinks(True)
         
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.about_label)
